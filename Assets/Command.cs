50558c5783ec12901a6252e9b2dd132a0c2b2efd
Brian David <164612324+Brian-David-Oeides@users.noreply.github.com>
2024-06-03 19:52:31 +0700

    Initial commit

---
+++ Assets/Command.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command
+{
+    public abstract void Execute(Animator anim, bool forward);
+}
+
+public class Advance : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if(forward)
+            anim.SetTrigger("isWalking");
+        else
+            anim.SetTrigger("isWalkingR");
+    }
+}
+
+public class TurnLeft : Command
+{
+    public float turnSpeed = 0.08f; // Adjust the turn speed as necessary
+
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+        {
+            RotateCharacter(anim, -6f, turnSpeed);
+            anim.SetTrigger("isWalking");
+        }
+    }
+
+    private void RotateCharacter(Animator anim, float angle, float speed)
+    {
+        Quaternion initialRotation = anim.transform.rotation;
+        Quaternion targetRotation = initialRotation * Quaternion.Euler(0, angle, 0);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < 1f)
+        {
+            elapsedTime += Time.deltaTime * speed;
+            anim.transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime);
+        }
+
+        anim.transform.rotation = targetRotation;
+    }
+}
+
+public class TurnRight : Command
+{
+    public float turnSpeed = 0.08f; // Adjust the turn speed as necessary, faster the turn speed the smaller degree of rotation
+
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+        {
+            RotateCharacter(anim, 6f, turnSpeed);
+            anim.SetTrigger("isWalking");
+        }
+    }
+
+    private void RotateCharacter(Animator anim, float angle, float speed)
+    {
+        Quaternion initialRotation = anim.transform.rotation;
+        Quaternion targetRotation = initialRotation * Quaternion.Euler(0, angle, 0);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < 1f)
+        {
+            elapsedTime += Time.deltaTime * speed;
+            anim.transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime);
+        }
+
+        anim.transform.rotation = targetRotation;
+    }
+}
+public class Jump: Command 
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+            anim.SetTrigger("isJumping");
+        else
+            anim.SetTrigger("isJumpingR");
+    }
+}
+
+public class Kick : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+            anim.SetTrigger("isKicking");
+        else
+            anim.SetTrigger("isKickingR");
+    }
+}
+
+public class Punch : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+            anim.SetTrigger("isPunching");
+        else
+            anim.SetTrigger("isPunchingR");
+    }
+}
+
+public class Retreat : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+            anim.SetTrigger("isWalkingR");
+        else
+            anim.SetTrigger("isWalking");
+    }
+}
+
+
+public class DoNothing: Command 
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+      
+    }
+}
+
+
---
+++ Assets/InputHandler.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public GameObject actor; // calls the game object
+    Animator anim;
+    Command keySpace, keyK, keyP, upArrow, downArrow, turnLeft, turnRight;
+    List<Command> oldCommands = new List<Command>();
+
+    Coroutine replayCoroutine;
+    bool shouldStartReplay;
+    bool isReplaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keySpace = new Jump(); // Keyword 'new' creates instance of original Command  
+        keyK = new Kick();
+        keyP = new Punch();
+        upArrow = new Advance();
+        downArrow = new Retreat();
+        turnLeft = new TurnLeft(); // Initialize new commands
+        turnRight = new TurnRight(); // Initialize new commands
+        anim = actor.GetComponent<Animator>();
+        Camera.main.GetComponent<CameraFollow360>().player = actor.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isReplaying)
+            HandleInput();
+        StartReplay();
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            keySpace.Execute(anim, true);
+            oldCommands.Add(keySpace);
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            keyK.Execute(anim, true);
+            oldCommands.Add(keyK);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            keyP.Execute(anim, true);
+            oldCommands.Add(keyP);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            upArrow.Execute(anim, true);
+            oldCommands.Add(upArrow);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            downArrow.Execute(anim, true);
+            oldCommands.Add(downArrow); // Record list of oldCommands
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow)) // Handle turn left
+        {
+            turnLeft.Execute(anim, true);
+            oldCommands.Add(turnLeft); // Record list of oldCommands
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) // Handle turn right
+        {
+            turnRight.Execute(anim, true);
+            oldCommands.Add(turnRight); // Record list of oldCommands
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+            shouldStartReplay = true;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            UndoLastCommand();
+
+    }
+
+    void UndoLastCommand()
+    {
+        if (oldCommands.Count > 0)
+        {
+            Command c = oldCommands[oldCommands.Count - 1];
+            c.Execute(anim, false);
+            oldCommands.RemoveAt(oldCommands.Count - 1); 
+        }
+    }
+
+    void StartReplay()
+    {
+        if(shouldStartReplay && oldCommands.Count > 0)
+        {
+            shouldStartReplay = false;
+            if(replayCoroutine != null)
+            {
+                StopCoroutine(replayCoroutine);
+            }
+            replayCoroutine = StartCoroutine(ReplayCommands());
+        }
+    }
+
+    IEnumerator ReplayCommands()
+    {
+        isReplaying = true;
+
+        for(int i = 0; i < oldCommands.Count; i++)
+        {
+            oldCommands[i].Execute(anim, true);
+            yield return new WaitForSeconds(0f);
+        }
+
+        isReplaying = false;
+    }
+}
